f1c137985acd25f47fa65927c7b2db4743b57066
Colter Ulrich <cju5025@gmail.com>
2021-11-30 22:47:17 -0700

    Writes ParameterSlider Class

--- FTChorusParameterSlider.cpp
+++ FTChorusParameterSlider.cpp
@@ -1,1 +1,20 @@
 #include "FTChorusParameterSlider.h"
+
+FTChorusParameterSlider::FTChorusParameterSlider(AudioProcessorValueTreeState& stateToControl,
+                    const String& parameterID)
+:   Slider(parameterID)
+{
+    setSliderStyle(SliderStyle::RotaryHorizontalVerticalDrag);
+    setTextBoxStyle(Slider::TextBoxBelow, false, 50, 20);
+    setColour(textBoxOutlineColourId, Colours::transparentWhite);
+    setLookAndFeel(&mDialLookAndFeel);
+    setRange(0.0f, 1.0f, 0.001f);
+    
+    mAttachment =
+    std::make_unique<AudioProcessorValueTreeState::SliderAttachment>(stateToControl, parameterID, *this);
+}
+
+FTChorusParameterSlider::~FTChorusParameterSlider()
+{
+    
+}
--- FTChorusParameterSlider.h
+++ FTChorusParameterSlider.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "JuceHeader.h"
+#include "FTChorusDial.h"
+
+class FTChorusParameterSlider
+:   public Slider
+{
+public:
+    
+    FTChorusParameterSlider(AudioProcessorValueTreeState& stateToControl,
+                        const String& parameterID);
+    
+    ~FTChorusParameterSlider();
+        
+private:
+    
+    std::unique_ptr<AudioProcessorValueTreeState::SliderAttachment> mAttachment;
+    FTChorusDial mDialLookAndFeel;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(FTChorusParameterSlider);
+};
